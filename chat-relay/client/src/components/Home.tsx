77cede31cae77c5b61e5e4c87391264c715a2ad5
Githendra Perera <gpayagalage@gmail.com>
2024-02-02 22:52:32 +0100

    [~] home.tsx - dark mode toggle function optimized

--- chat-relay/client/src/components/Home.tsx
+++ chat-relay/client/src/components/Home.tsx
@@ -19,7 +19,7 @@ const Home = () => {
       socket.emit("disconnect");
     }
 
-      socket.on("connect", () => {
+    socket.on("connect", () => {
       console.log("Connected to server:", socket.id);
     });
   }, []);
@@ -29,97 +29,56 @@ const Home = () => {
     toast.success("Logged out");
     localStorage.removeItem("username");
     setConnected(isConnected());
-
   };
 
-  var home = document.getElementById('home');
-  var theme=document.cookie
-  var currentTheme=""
-  if (theme=="theme=dark")
-    {
-    currentTheme="dark"
-    }
-  else(currentTheme="light")
-
-  async function loadTheme(){
-    home = document.getElementById('home')
-    console.log(document.readyState)
-  if (currentTheme=="dark")
-    {
-    // currentTheme="dark"
-    home?.classList.add("dark")
-    console.log("dark")
-    }
-    else
-    {
-      // currentTheme="light"
-      home?.classList.remove("dark")
-      console.log("light")
-    }}
-
-
-  function changeTheme()
-  {
-    home = document.getElementById('home');
-    theme = document.cookie
-    if (currentTheme=="dark")
-    {
-    document.cookie = "theme=light"
-    currentTheme="light"
-    home?.classList.remove("dark")
-    }
-    else
-    {
-      document.cookie="theme=dark"
-      currentTheme="dark"
-      home?.classList.add("dark")
-    }
+  function toggleTheme() {
+    document.documentElement.classList.toggle("dark");
   }
 
-document.addEventListener('keypress',loadTheme)
-
   return (
+    <div>
+      {connected ? (
+        <div className="flex flex-row bg-black h-[100vh]">
+          <div className="w-1/5 m-2 text-black dark:text-[#09ebe3] dark:bg-[#03252b] bg-white rounded flex flex-col justify-center">
+            <Channel />
+          </div>
 
-    <div id="home" onMouseEnter={loadTheme}>
-      <div>
-        {connected ? (
-          <div className="flex flex-row bg-black h-[100vh]">
-            <div className="w-1/5 m-2 text-black dark:text-[#09ebe3] dark:bg-[#03252b] bg-white rounded flex flex-col justify-center">
-              <Channel />
+          <div className="w-3/5 my-2 flex flex-col text-black dark:text-[#09ebe3] dark:bg-[#05323a] bg-white rounded justify-center items-start">
+            <div className="flex justify-between">
+              <h1 className="text-3xl font-bold text-black">
+                Welcome {username}
+              </h1>
+
+              <button
+                onClick={toggleTheme}
+                className="dark:bg-white dark:text-black bg-[#004449] text-[#09ebe3]"
+              >
+                change theme
+              </button>
+
+              <button
+                className="p-2 bg-red-500 hover:bg-red-700 text-white rounded"
+                onClick={logOut}
+              >
+                Logout
+              </button>
             </div>
 
-            <div className="w-3/5 my-2 flex flex-col text-black dark:text-[#09ebe3] dark:bg-[#05323a] bg-white rounded justify-center items-start">
-              <div className="flex justify-between">
-                <h1 className="text-3xl font-bold text-black">
-                  Welcome {username}
-                </h1>
+            <div className="mt-2 flex flex-col w-full">
+              <ChatBody />
 
-                <button id="themetoggle" onClick={changeTheme} className="dark:bg-white dark:text-black bg-[#004449] text-[#09ebe3]">change theme</button>
-
-                <button
-                  className="p-2 bg-red-500 hover:bg-red-700 text-white rounded"
-                  onClick={logOut}
-                >
-                  Logout
-                </button>
+              <div className="mt-2 p-2">
+                <Input />
               </div>
-
-              <div className="mt-2 flex flex-col w-full">
-                <ChatBody />
-
-                <div className="mt-2 p-2">
-                  <Input />
-                </div>
-              </div>
-            </div>
-            <div className="w-1/5 m-2 text-black dark:text-[#09ebe3] dark:bg-[#03252b] bg-white rounded flex justify-center">
-              <ActiveUser />
             </div>
           </div>
-        ) : (
-          <App />
-        )}
-      </div>
+          <div className="w-1/5 m-2 text-black dark:text-[#09ebe3] dark:bg-[#03252b] bg-white rounded flex justify-center">
+            <ActiveUser />
+          </div>
+        </div>
+      ) : (
+        <App />
+      )}
     </div>
   );
 };
