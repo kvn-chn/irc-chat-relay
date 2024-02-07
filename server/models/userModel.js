c77e2e224ab47a49ff3ac31394515fcbfa1b9464
Githendra Perera <gpayagalage@gmail.com>
2024-02-07 20:21:44 +0100

    [+] added bcrypt to hash password & check missing and wrong fields

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secretKey = 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcwNjY0MTUzNSwiaWF0IjoxNzA2NjQxNTM1fQ.Mj7cixwuIdP6rCNQ_6riQWoXa6WkNPYhoXmXwo4ptVs';
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -9,9 +12,58 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    socket_id: {
+        type: String,
+        unique: true,
+        required: true
     }
-}, {timestamps: true});
+}, { timestamps: true });
 
 const User = mongoose.model('User', UserSchema);
+
+User.prototype.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+User.prototype.hashPassword = async function (password) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        return hashedPassword;
+    } catch (error) {
+        throw error;
+    }
+};
+
+User.prototype.generateToken = function () {
+    try {
+        const token = jwt.sign(
+            { username: this.username },
+            secretKey,
+            { expiresIn: '1h' }
+        );
+
+        return token;
+    } catch (err) {
+        throw err;
+    }
+};
+
+User.prototype.getInfoFromToken = function (token) {
+    try {
+        const decoded = jwt.verify(token, secretKey);
+
+        if (decoded) {
+            const username = decoded.username;
+
+            return { username };
+        }
+        return null;
+    } catch (err) {
+        throw err;
+    }
+};
 
 module.exports = User;
--- server/routes/user.js
+++ server/routes/user.js
@@ -6,18 +6,44 @@ const router = express.Router();
 const jwtSecret = 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcwNjY0MTUzNSwiaWF0IjoxNzA2NjQxNTM1fQ.Mj7cixwuIdP6rCNQ_6riQWoXa6WkNPYhoXmXwo4ptVs';
 
 router.post('/register', async (req, res) => {
-    const { username, password } = req.body;
     try {
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            const missingFields = [];
+
+            if (!username) missingFields.push('Username');
+            if (!username) missingFields.push('Password');
+
+            return res.status(400).json({
+                message: `Please provide data to create a user. Missing fields: ${missingFields.join(', ')}.`
+            });
+        }
+        else if (typeof username !== 'string' || typeof password !== 'string') {
+            const wrongFields = [];
+
+            if (typeof username !== 'string') wrongFields.push('username');
+            if (typeof password !== 'string') wrongFields.push('password');
+
+            return res.status(400).json({
+                message: `Please provide the correct types to create an employer. Wrong fields: ${wrongFields.join(', ')}.`
+            });
+        }
+
+        const existingUser = await User.findOne({ where: { username } }) 
+        
+        if (existingUser) return res.status(400).json({ message: 'Email already used' });
+
+    
         const createdUser = await User.create({ username, password });
+        
         jwt.sign({ userId: createdUser._id, username }, jwtSecret, {} ,(err, token) => {
-        if (err) throw err;
-        res.cookie('token', token).status(201).json({
-        id: createdUser._id,
+            if (err) throw err;
+            res.cookie('token', token).status(201).json({id: createdUser._id, username: username, password: password });
         });
-    });
     } catch (err) {
-        if (err) throw err;
-        res.status(500).json({ error: 'Failed to create user' });
+        console.error(error);
+        return res.status(500).json({ message: 'Internal Server Error' });
     }
 });
 
